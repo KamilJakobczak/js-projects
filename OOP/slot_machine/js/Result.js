cf5b29858723f30af970801db47ef0e4e7c41ebf
Kamil Jakóbczak <k_j1@vp.pl>
2025-02-18 10:40:56 +0100

    create class Result

---
+++ OOP/slot_machine/js/Result.js
@@ -0,0 +1,17 @@
+class Result {
+	static moneyWinInGame(result, bid) {
+		if (result) return 3 * bid;
+		else return 0;
+	}
+	static checkWinner(draw) {
+		const a = draw[0];
+		const b = draw[1];
+		const c = draw[2];
+		if (
+			(a === b && a === c) ||
+			(a !== b && b !== c && a !== c)
+		) {
+			return true;
+		} else return false;
+	}
+}
