525c539907471c38d38ca72adefce9919d10ce2e
Kamil Jakóbczak <k_j1@vp.pl>
2025-02-21 10:25:44 +0100

    add photo editor script

---
+++ other/photo-edit/index.js
@@ -0,0 +1,19 @@
+const brightnessBar = document.getElementById('brightness-bar');
+const contrastBar = document.getElementById('contrast-bar');
+const saturationBar = document.getElementById('saturation-bar');
+const hueBar = document.getElementById('hue-bar');
+const blurBar = document.getElementById('blur-bar');
+const image = document.getElementById('image');
+const root = document.documentElement;
+
+const updateFilters = () => {
+  root.style.setProperty('--brightness', brightnessBar.value + '%');
+  root.style.setProperty('--contrast', contrastBar.value + '%');
+  root.style.setProperty('--saturation', saturationBar.value + '%');
+  root.style.setProperty('--hue', hueBar.value + 'deg');
+  root.style.setProperty('--blur', blurBar.value + 'px');
+};
+
+[brightnessBar, contrastBar, saturationBar, hueBar, blurBar].forEach((bar) => {
+  bar.addEventListener('input', updateFilters);
+});
